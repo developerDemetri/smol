ec5a8a30a5437c307331573ce32a1994010f259e
Demetrius Shargani <developerdemetri@gmail.com>
2021-03-14 12:31:30 -0700

    place Lambda in private subnets

--- smol_cdk/app.py
+++ smol_cdk/app.py
@@ -1,8 +1,16 @@
-from aws_cdk.core import App
+from os import environ
+
+from aws_cdk.core import App, Environment
 
 from smol_cdk.stack import SmolCdkStack
 
 
 app = App()
-SmolCdkStack(app, "SmolCdkStack")
+SmolCdkStack(
+    app,
+    "SmolCdkStack",
+    env=Environment(
+        account=environ["CDK_ACCOUNT"], region=environ.get("CDK_REGION", "us-west-2")
+    ),
+)
 app.synth()
